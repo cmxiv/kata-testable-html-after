6daef88ed638ac095ffe3bbbcc6bef33c4a5f45e
Siddhant Tandon <cmxiv914@gmail.com>
2023-07-21 04:34:15 +0200

    extract mode variable

--- src/main/kotlin/dev/tandosid/cleancode/kotlin/TestableHtml.kt
+++ src/main/kotlin/dev/tandosid/cleancode/kotlin/TestableHtml.kt
@@ -20,28 +20,30 @@ private class TestableHtmlMaker(private val pageData: PageData, private val incl
 
     fun make(): String {
         if (pageData.hasAttribute("Test")) {
+            val mode = "setup"
             if (includeSuiteSetup) {
                 val suiteSetup: WikiPage? = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_SETUP_NAME, wikiPage)
                 if (suiteSetup != null) {
                     val pagePath = wikiPage.pageCrawler.getFullPath(suiteSetup)
                     val pagePathName = PathParser.render(pagePath)
-                    buffer.append("!include -setup .").append(pagePathName).append("\n")
+                    buffer.append("!include -$mode .").append(pagePathName).append("\n")
                 }
             }
             val setup: WikiPage? = PageCrawlerImpl.getInheritedPage("SetUp", wikiPage)
             if (setup != null) {
                 val setupPath = wikiPage.pageCrawler.getFullPath(setup)
                 val setupPathName = PathParser.render(setupPath)
-                buffer.append("!include -setup .").append(setupPathName).append("\n")
+                buffer.append("!include -$mode .").append(setupPathName).append("\n")
             }
         }
         buffer.append(pageData.content)
         if (pageData.hasAttribute("Test")) {
+            val mode = "teardown"
             val teardown: WikiPage? = PageCrawlerImpl.getInheritedPage("TearDown", wikiPage)
             if (teardown != null) {
                 val tearDownPath = wikiPage.pageCrawler.getFullPath(teardown)
                 val tearDownPathName = PathParser.render(tearDownPath)
-                buffer.append("!include -teardown .").append(tearDownPathName).append("\n")
+                buffer.append("!include -$mode .").append(tearDownPathName).append("\n")
             }
             if (includeSuiteSetup) {
                 val suiteTeardown: WikiPage? =
@@ -49,7 +51,7 @@ private class TestableHtmlMaker(private val pageData: PageData, private val incl
                 if (suiteTeardown != null) {
                     val pagePath = wikiPage.pageCrawler.getFullPath(suiteTeardown)
                     val pagePathName = PathParser.render(pagePath)
-                    buffer.append("!include -teardown .").append(pagePathName).append("\n")
+                    buffer.append("!include -$mode .").append(pagePathName).append("\n")
                 }
             }
         }
